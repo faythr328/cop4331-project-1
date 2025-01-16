3212eb707621da479ed0194e620d30f5094ea3e0
Keola <keolamcgowan97@outlook.com>
2025-01-15 20:28:45 -0500

    initial project files

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$username = "dbmanager";
+$password = "Ilovedb4331";
+$dbname = "COP4331db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+ }
+
+echo "Connection success<br>" ;
+
+?>

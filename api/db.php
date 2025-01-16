dc21f49773ea8bf4469a5e465b917e509b5622c0
enwask <enwask@ucf.edu>
2025-01-15 20:35:05 -0500

    move api + reformat

--- api/db.php
+++ api/db.php
@@ -8,9 +8,10 @@ $dbname = "COP4331db";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-   die("Connection failed: " . $conn->connect_error);
- }
+    die("Connection failed: " . $conn->connect_error);
+}
 
-echo "Connection success<br>" ;
+echo "Connection success<br>";
 
 ?>
+
---
+++ api/login.php
@@ -0,0 +1,40 @@
+<?php
+//start/resume session (access session data) 
+session_start();
+include 'db.php';
+//connection error check
+if ($conn->connect_error) {
+    die("Connection failed" . $conn->connect_error);
+}
+//check if form is sumbmitted 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $Login = $_POST['Login'];
+    $Password = $_POST['Password'];
+}
+
+//prepare SQL statement 
+$stmt = $conn->prepare("SELECT ID, Password 
+	 		 FROM Users
+ 			 WHERE Login = ?");
+$stmt->bind_param("s", $Login);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows === 1) {
+    $row = $result->fetch_assoc();
+    //verify password
+    if ($Password === $row['Password']) {
+        $_SESSION['ID'] = $row['ID'];
+        header("Location: home.html ");
+        exit();
+    } else {
+        $error = "Invalid Password";
+    }
+} else {
+    $error = "Invalid Login";
+}
+
+$stmt->close();
+$conn->close();
+
+?>

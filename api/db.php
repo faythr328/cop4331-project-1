56543bd1a1c29e059d7918f92962bfc1590cbfd8
enwask <enwask@ucf.edu>
2025-01-21 15:39:11 -0500

    remove extraneous error handling

--- api/db.php
+++ api/db.php
@@ -7,11 +7,4 @@ $dbname = "COP4331db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-echo "Connection success<br>";
-
 ?>
-
